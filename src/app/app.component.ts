e35d8359552aefd0a93883d8555f9df7abae79db
Sulaiman Mah <sulaiman.mah@gmail.com>
2021-09-01 18:07:47 +1000

    Hero section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.style.css'] // reference to the style file.
+
+  
 })
 export class AppComponent {
   title = 'smSite';
